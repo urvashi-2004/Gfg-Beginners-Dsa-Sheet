18e7de27e69df6d1b256f10cf6eaf31e5ba3be21
urvashi-2004 <85677323+urvashi-2004@users.noreply.github.com>
2023-10-06 22:15:19 +0530

    day1

---
+++ min_and_max_in_array.cpp
@@ -0,0 +1,17 @@
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+      
+      long long int min=a[0];
+   long long int max=a[0];
+   
+   for(int i=0;i<n;i++){
+       if(a[i]>max){
+           max=a[i];
+       }
+       if(a[i]<min){
+           min=a[i];
+       }
+   }
+   
+   return {min,max};
+}
