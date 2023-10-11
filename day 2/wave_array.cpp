05865c972d3f21218152b24bfc40cfdf67724e4a
urvashi-2004 <85677323+urvashi-2004@users.noreply.github.com>
2023-10-11 19:31:45 +0530

    day 2

---
+++ day 2/wave_array.cpp
@@ -0,0 +1,10 @@
+class Solution{
+    public:
+    void convertToWave(int n, vector<int>& arr){
+        
+        for(int i=0;i<n-1;i=i+2){
+            swap(arr[i],arr[i+1]);
+        }
+        
+    }
+};
